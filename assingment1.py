d6c84aef9ee803fe043a527a9d0b5743a4187dea
Sandeep <sk1988908@gmail.com>
2018-06-12 03:44:18 -0700

    first assignment submitted

--- assingment1.py
+++ assingment1.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+FOR INITIALIZATION
+git config--global user.name"sandeep kumar"
+git config--global user.email"sk1988908@gmail.com"
+
+git init
+git add assignement1.py
+git add .
+git status
+git commit-m"assignement1 is submitted"
+git log.
+git remote add origin
+git push origin master
