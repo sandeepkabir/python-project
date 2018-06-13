6e18fe3bf424abd9df80e45594bb028f1d9a011f
Sandeep <sk1988908@gmail.com>
2018-06-12 23:55:16 -0700

    assignement1 is submitted

---
+++ assingment2.py
@@ -0,0 +1,28 @@
+#Q1
+
+print("my name is sandeep")
+
+
+#Q2
+print("sandeep" +"kabir")
+
+#Q3
+
+x=input("enter any number")
+y=input("enter any number")
+z=input("enter any number")
+print(x,y,z)
+
+
+
+#Q4
+
+print("let\'s get started")
+
+#Q5
+
+s="acadview"
+course="python"
+fees=5000
+print("%s %s %d" %(s,course,fees))
+print (s + " " + course + " " + str(fees))
