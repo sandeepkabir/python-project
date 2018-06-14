c63b4a601f049c9b4fe5ba1a5f514b74f6e108a8
Sandeep <sk1988908@gmail.com>
2018-06-13 23:25:11 -0700

    assignement3 submitted

---
+++ assignement3.py
@@ -0,0 +1,75 @@
+#Q1
+l=[1,3,45,6,2]
+a=input("enter the value")
+b=input("enter the value")
+c=input("enter the value")
+d=input("enter the value")
+e=input("enter the value")
+l=[a,b,c,d,e]
+print(l)
+
+
+#Q2
+
+l2=['google','apple','facebook','microsoft','tesla']
+
+print(l.extend(l2))
+
+print(l)
+
+
+#Q3
+
+l=[1,2,3,1,4,1,5,1]
+
+print(l.count(1))
+
+
+#Q4
+
+l=[33,1,66,44,22]
+
+l.sort()
+
+print(l)
+
+
+
+
+#Q5
+A=[1,2,3,7,10]
+B=[5,6,8,9]
+print(A.extend(B))
+C=A
+C.sort()
+print(c)
+
+
+
+#Q6
+#for stack
+
+a=input("enter value")
+b=input("enter value")
+c=input("enter value")
+d=input("enter value")
+e=input("enter value")
+l = [a,b,c,d,e]
+print(l.pop())
+print(l)
+
+
+
+#for queue
+
+a=input("enter value")
+b=input("enter value")
+c=input("enter value")
+d=input("enter value")
+e=input("enter value")
+l=[a,b,c,d,e]
+
+l.reverse()
+print(l.pop())
+
+print(l)
